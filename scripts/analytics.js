521ae64feb4b01a465d26d2138af586082e0be67
GloryPkqa <glorypkqa@163.com>
2024-02-18 10:40:47 +0800

    更新

--- scripts/analytics.js
+++
@@ -1,9 +0,0 @@
-hexo.extend.injector.register('body_end', `<script>
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?8b9e764f9a09501fea21f4eb6ad131a2";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
-</script>`);
