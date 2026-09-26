d87df3b8caa631ab83fe057ae044257efb956abe
GloryPkqa <glorypkqa@gmail.com>
2026-09-26 11:54:30 +0800

    Add BandBBS profile link

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -2,5 +2,6 @@ export const socials = [
   { name: "GitHub", detail: "@GloryPkqa", href: "https://github.com/GloryPkqa", arrow: "↗", external: true },
   { name: "Bilibili", detail: "来看看我的动态", href: "https://space.bilibili.com/1657868367", arrow: "↗", external: true },
   { name: "网易云音乐", detail: "最近在听什么", href: "https://music.163.com/#/user/home?id=8146087493", arrow: "↗", external: true },
+  { name: "米坛", detail: "我的米坛主页", href: "https://www.bandbbs.cn/members/1089317/", arrow: "↗", external: true },
   { name: "Email", detail: "glorypkqa@gmail.com", href: "mailto:glorypkqa@gmail.com", arrow: "→", external: false },
 ] as const;
